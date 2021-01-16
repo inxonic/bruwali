99176012c7d5f695da19fc95089b3f251f186823
Daniel Balko <inxonic@users.noreply.github.com>
2021-01-17 00:12:01 +0100

    Increase code readability and style compliance
    - use constants instead of macros
    - simplify expressions
    - add comments

--- bruwali.c
+++ bruwali.c
@@ -3,18 +3,27 @@
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 
-#define AUTO_OFF_TIME 600
+// This is the time in seconds after which the device turns off automatically.
+static const int kAutoOffTime = 600;
 
-#define BRIGHTNESS_STEP_A 40
-#define BRIGHTNESS_STEP_B 32
+// These are the step sizes by which the brightness is changed on every timer
+// overflow for the tractor and the trailer in revolving light mode. A higher
+// value results in a faster rotation speed.
+static const int kTractorBrightnessStep = 40;
+static const int kTrailerBrightnessStep = 32;
 
-#define FLASH_COUNT 3
-#define FLASH_REPEAT 43
+// This is the number of times the trailer light flashes in flashing mode.
+static const int kTrailerFlashCount = 3;
+// This is the number of timer overflows after which the trailer light starts to
+// flash again in flashing mode. Prime numbers should give good results.
+static const int kTrailerFlashInterval = 43;
 
 static volatile uint8_t mode __attribute__((section(".noinit")));
 static uint16_t auto_off_timer;
 
-// https://www.nongnu.org/avr-libc/user-manual/group__avr__watchdog.html
+// We need to turn off the watchdog during early program startup as it remains
+// active after a system reset
+// [https://www.nongnu.org/avr-libc/user-manual/group__avr__watchdog.html].
 uint8_t mcusr_mirror __attribute__((section(".noinit")));
 
 void get_mcusr(void) __attribute__((naked)) __attribute__((section(".init3")));
@@ -25,49 +34,54 @@ void get_mcusr(void) {
 }
 
 int main(void) {
-  // Turn off everything we don't need to save energy
+  // We turn off everything we don't need to save energy.
   PRR = _BV(PRTWI) | _BV(PRTIM0) | _BV(PRTIM1) | _BV(PRSPI) | _BV(PRUSART0) |
         _BV(PRADC);
 
   mode++;
 
-  // Only accept external reset source to activate the device
+  // After a watchdog or power on reset the device enters the off state.
   if (mcusr_mirror & (_BV(WDRF) | _BV(PORF))) {
-    mode = 255;
+    mode = UINT8_MAX;
   }
 
-  // Handle operational modes
+  // These are the operational modes.
   if (mode <= 4) {
-    // Enable the step-up converter
+    // Enable the step-up converter.
     PORTB |= _BV(PORTB0);
     DDRB |= _BV(DDB0);
 
-    auto_off_timer = AUTO_OFF_TIME / 8;
-
+    // Use the watchdog interrupt with an 8 seconds interval to
+    // automatically turn off the device after the specified time.
+    auto_off_timer = kAutoOffTime / 8;
     wdt_reset();
     WDTCSR |= _BV(WDCE) | _BV(WDE);
     WDTCSR = _BV(WDIE) | _BV(WDP3) | _BV(WDP0);
 
     cli();
 
-    // Prescaler at 8 lets the timer overflow at 62.5 Hz
+    // A prescaler value of 8 lets the timer overflow at 62.5 Hz.
     TCCR2B = _BV(CS21);
 
     if (mode == 1 || mode == 3) {
+      // Revolving light on the tractor.
       GPIOR0 = 0b01100011;
       GPIOR2 |= _BV(0);
       TIMSK2 |= _BV(OCIE2A) | _BV(TOIE2);
     } else {
+      // Flashing light on the tractor.
       GPIOR0 = 0;
       GPIOR2 &= ~_BV(0);
       TIMSK2 |= _BV(TOIE2);
     }
 
     if (mode == 1 || mode == 2) {
+      // Revolving light on the trailer.
       GPIOR1 = 0b11111001;
       GPIOR2 |= _BV(1);
       TIMSK2 |= _BV(OCIE2B) | _BV(TOIE2);
     } else {
+      // Flashing light on the trailer.
       GPIOR1 = 0;
       GPIOR2 &= ~_BV(1);
       TIMSK2 |= _BV(TOIE2);
@@ -82,9 +96,9 @@ int main(void) {
     set_sleep_mode(SLEEP_MODE_PWR_SAVE);
   }
 
-  // Every other mode value puts the device to the power down mode
+  // Every other mode value puts the device into the off state.
   else {
-    // Ensure everything is off, though redundant after a reset
+    // Ensure everything is off, though this is redundant after a reset.
     cli();
     TIMSK2 = 0;
     TCCR2B = 0;
@@ -97,11 +111,12 @@ int main(void) {
     PORTC = 0;
     PORTD = 0;
 
-    // Disable the step-up converter
+    // Disable the step-up converter.
     DDRB = _BV(DDB0);
     PORTB &= ~_BV(PORTB0);
 
-    // Next external reset will start first operational mode
+    // The next external reset will put the device into the first operational
+    // mode.
     mode = 0;
 
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
@@ -114,7 +129,7 @@ int main(void) {
 
 ISR(WDT_vect) {
   if (--auto_off_timer == 0) {
-    // Let the next watchdog timeout reset the device
+    // The next watchdog timeout will reset the device.
     wdt_enable(WDTO_15MS);
   }
 }
@@ -139,15 +154,17 @@ ISR(TIMER2_OVF_vect) {
   uint8_t a = GPIOR0;
 
   if (GPIOR2 & _BV(0)) {
+    // Revolving light on the tractor.
     uint8_t b = a << 1 | a >> 4 & 1 << 0;
 
     PORTD = b;
     PORTB = PORTB & 0b00111111 | b << 3 & 0b11000000;
 
-    if ((OCR2A += BRIGHTNESS_STEP_A) < BRIGHTNESS_STEP_A) {
+    if ((OCR2A += kTractorBrightnessStep) < kTractorBrightnessStep) {
       GPIOR0 = b;
     }
   } else {
+    // Flashing light on the tractor.
     if (a >> 4 == 6 || (a & 1 << 0) == 0) {
       if (a >> 4 == 6) {
         PORTD = 0;
@@ -174,24 +191,24 @@ ISR(TIMER2_OVF_vect) {
         }
       }
     }
-    a += 1 << 4;
-    a++;
-    GPIOR0 = a;
+    GPIOR0 = a + (1 << 4) + 1;
   }
 
   uint8_t k = GPIOR1;
 
   if (GPIOR2 & _BV(1)) {
+    // Revolving light on the trailer.
     uint8_t l = k << 1 | k >> 4 & 1 << 1;
 
     PORTB = PORTB & 0b11111001 | l & 0b00000110;
     PORTC = PORTC & 0b11000111 | l & 0b00111000;
 
-    if ((OCR2B += BRIGHTNESS_STEP_B) < BRIGHTNESS_STEP_B) {
+    if ((OCR2B += kTrailerBrightnessStep) < kTrailerBrightnessStep) {
       GPIOR1 = l;
     }
   } else {
-    if ((k & 1 << 0) == 0 && (k >> 2) < FLASH_COUNT) {
+    // Flashing light on the trailer.
+    if ((k & 1 << 0) == 0 && (k >> 2) < kTrailerFlashCount) {
       if ((k >> 1 & 1 << 0) == 0) {
         PORTB &= ~0b00000110;
         PORTC &= ~0b00111000;
@@ -201,7 +218,7 @@ ISR(TIMER2_OVF_vect) {
       }
     }
     k++;
-    if (k == FLASH_REPEAT) {
+    if (k == kTrailerFlashInterval) {
       k = 0;
     }
     GPIOR1 = k;
